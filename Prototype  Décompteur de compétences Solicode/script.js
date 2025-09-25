049066fda9693564afda81bb4c902ee94e52be7d
ALAE MAGHCHICH <alaemaghchich2004@gmail.com>
2025-09-25 09:06:02 +0100

    script.js

--- Prototype  Décompteur de compétences Solicode/script.js
+++ Prototype  Décompteur de compétences Solicode/script.js
@@ -1,9 +1,12 @@
+function alae(){
 let competences = ["C1", "C3", "C2", "C1", "C8", "C1"];
-let codeChoisi = "C1"; //SELECTED an COD BY RESERCH IN COMPETENCES and his c1
-let compteur = 0; //create variable his name compteur = 0 becous start calcul from 0
-for (let i = 0; i < competences.length; i++) {  //Here we started loop cous returne all component =العناصر  
-  if (competences[i] == codeChoisi) {
+let codeChoisi = "C1";
+let compteur = 0;
+for (let i = 0; i < competences.length; i++) {  
+  if (competences[i] === codeChoisi) {
     compteur++;
   }
 }
-console.log("Le code " + codeChoisi + " apparaît " + compteur );
+console.log(codeChoisi + " = " + compteur );
+}
+alae();
